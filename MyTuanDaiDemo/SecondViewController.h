a9373ee35d4031dd21dd246ac2e8ed850bb70da1
linxinjia <linxinjia@linxinjiadeiMac.local>
2016-12-27 15:13:06 +0800

    secondViewController

---
+++ MyTuanDaiDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  MyTuanDaiDemo
+//
+//  Created by linxinjia on 2016/12/27.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
